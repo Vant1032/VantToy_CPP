9fd4241f53882e84327f837e3329f45943e7f841
Vant <forvant@qq.com>
2018-09-28 13:58:08 +0800

    测试fork

--- TestFork.cpp
+++ TestFork.cpp
@@ -2,3 +2,25 @@
 // Created by Vant on 18-9-28.
 //
 
+#include <cstdio>
+#include <unistd.h>
+
+int main() {
+    printf("开始运行\n");
+    pid_t pid = fork();
+    int t;
+    switch (pid) {
+        case -1:
+            perror("fork 出现错误\n");
+            break;
+        case 0:
+            printf("我是子进程\n");
+            t = execl("/bin/touch", "-c", "/home/touched.txt2", nullptr);
+            perror("");
+            break;
+        default:
+            printf("我是父进程,子进程pid = %d\n", pid);
+            break;
+    }
+
+}
